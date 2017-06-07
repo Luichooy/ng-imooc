e1a255030baf62520afaeaede2875595574336ef
luichooy <luichooy@163.com>
2017-06-07 15:05:39 +0800

    :-)

--- build/js/index.js
+++ build/js/index.js
@@ -149,157 +149,6 @@ app.config(['$validationProvider',function($validationProvider){
     $validationProvider.setExpression(expression).setDefaultMsg(defaultMsg);
 }]);
 
-app.directive('appFooter',function(){
-    return {
-        restrict:'E',
-        replace:true,
-        templateUrl:'view/template/footer.html'
-    };
-});
-
-app.directive('appHeader',function(){
-    return {
-        restrict:'E',
-        replace:true,
-        templateUrl:'view/template/header.html'
-    }
-});
-
-app.directive('mainList',function(){
-    return {
-        restrict:'E',
-        replace:true,
-        templateUrl:"view/template/mainList.html",
-        scope: {
-            data: '=',
-            filterObj: '='
-        }
-    };
-});
-
-app.directive('posClass',[function(){
-    return {
-        restrict: 'E',
-        replace: true,
-        templateUrl: 'view/template/posClass.html',
-        link: function($scope){
-            $scope.showPositionList = function(index){
-                $scope.isActive = index;
-
-                $scope.positionList = $scope.company.positionClass[index].positionList;
-            };
-
-            //此时$scope对象中的company属性还没有建立起来，因此通过$watch来检测$scope.company。
-            $scope.$watch('company',function(newVal,oldVal){
-                console.log(newVal);
-                if(newVal){
-                    $scope.showPositionList(0);
-                }
-            });
-        }
-    };
-}]);
-
-app.directive('posCompany',[function(){
-    return {
-        restrict: 'E',
-        replace: true,
-        templateUrl: 'view/template/posCompany.html',
-        scope:{
-            company: '='
-        }
-    };
-}]);
-
-app.directive('posHeader',[function(){
-    return {
-        restrict: 'E',
-        replace: true,
-        templateUrl: 'view/template/posHeader.html',
-        scope: {
-            title: '@'
-        },
-        link: function(scope,element,attr){
-            scope.back = function(){
-                window.history.back();
-            };
-        }
-    };
-}]);
-
-app.directive('posInfo',[function(){
-    return {
-        restrict: 'E',
-        replace: true,
-        templateUrl: 'view/template/posInfo.html',
-        scope: {
-            isActive:'=',
-            isLogin: '=',
-            position: '='
-        },
-        link: function($scope){
-            $scope.imagePath = $scope.isActive ? 'image/star-active.png' : 'image/star.png';
-        }
-    };
-}]);
-
-app.directive('searchTab',[function(){
-    return {
-        restrict: 'E',
-        replace: true,
-        templateUrl: 'view/template/searchTab.html',
-        scope: {
-            list: '=',
-            tabClick: '&'
-        },
-        link: function($scope){
-            $scope.activeId = $scope.list[0].id;
-
-            $scope.clickTab = function(tab){
-                $scope.activeId = tab.id;
-                $scope.tabClick(tab);
-            }
-        }
-    };
-}]);
-
-app.directive('sheet',[function(){
-    return {
-        restrict: 'E',
-        replace: true,
-        templateUrl: 'view/template/sheet.html',
-        scope: {
-            data: '=',
-            visible: '=',
-            select: '&'
-        },
-        link: function($scope){
-            $scope.hideSheet = function(){
-                $scope.visible = false;
-            };
-        }
-    };
-}]);
-
-app.filter('filterByObj',[function(){
-    return function(list,obj){
-        var result = [];
-        angular.forEach(list,function(item){
-
-            var isEqual = true;
-            for(var attr in obj){
-                if(item[attr] !== obj[attr]){
-                    isEqual = false;
-                }
-            }
-            if(isEqual){
-                result.push(item);
-            }
-        });
-        return result;
-    };
-}]);
-
 app.controller('companyCtrl',['$scope','$http','$state',function($scope,$http,$state){
     $http.get('data/company.json?id='+$state.params.id)
         .success(function(res){
@@ -522,6 +371,156 @@ app.controller('searchCtrl',['$scope','$http','dict',function($scope,$http,dict)
     };
 }]);
 
+app.directive('appFooter',function(){
+    return {
+        restrict:'E',
+        replace:true,
+        templateUrl:'view/template/footer.html'
+    };
+});
+
+app.directive('appHeader',function(){
+    return {
+        restrict:'E',
+        replace:true,
+        templateUrl:'view/template/header.html'
+    }
+});
+
+app.directive('mainList',['$http',function($http){
+    return {
+        restrict:'E',
+        replace:true,
+        templateUrl:"view/template/mainList.html",
+        scope: {
+            data: '=',
+            filterObj: '=',
+            isFavorite: '='
+        },
+        link: function($scope,$http){
+
+            $scope.select = function(item,$event){
+
+                // $http.post('data/favorite.json',{
+                //     id: item.id,
+                //     select: !item.select
+                // }).then(function(res){
+                //     console.log(res);
+                //     item.select = !item.select;
+                // },function(err){
+                //     console.log(err)
+                // });
+                item.select = !item.select;
+                $event.stopPropagation();
+            };
+        }
+    };
+}]);
+
+app.directive('posClass',[function(){
+    return {
+        restrict: 'E',
+        replace: true,
+        templateUrl: 'view/template/posClass.html',
+        link: function($scope){
+            $scope.showPositionList = function(index){
+                $scope.isActive = index;
+
+                $scope.positionList = $scope.company.positionClass[index].positionList;
+            };
+
+            //此时$scope对象中的company属性还没有建立起来，因此通过$watch来检测$scope.company。
+            $scope.$watch('company',function(newVal,oldVal){
+                console.log(newVal);
+                if(newVal){
+                    $scope.showPositionList(0);
+                }
+            });
+        }
+    };
+}]);
+
+app.directive('posCompany',[function(){
+    return {
+        restrict: 'E',
+        replace: true,
+        templateUrl: 'view/template/posCompany.html',
+        scope:{
+            company: '='
+        }
+    };
+}]);
+
+app.directive('posHeader',[function(){
+    return {
+        restrict: 'E',
+        replace: true,
+        templateUrl: 'view/template/posHeader.html',
+        scope: {
+            title: '@'
+        },
+        link: function(scope,element,attr){
+            scope.back = function(){
+                window.history.back();
+            };
+        }
+    };
+}]);
+
+app.directive('posInfo',[function(){
+    return {
+        restrict: 'E',
+        replace: true,
+        templateUrl: 'view/template/posInfo.html',
+        scope: {
+            isActive:'=',
+            isLogin: '=',
+            position: '='
+        },
+        link: function($scope){
+            $scope.imagePath = $scope.isActive ? 'image/star-active.png' : 'image/star.png';
+        }
+    };
+}]);
+
+app.directive('searchTab',[function(){
+    return {
+        restrict: 'E',
+        replace: true,
+        templateUrl: 'view/template/searchTab.html',
+        scope: {
+            list: '=',
+            tabClick: '&'
+        },
+        link: function($scope){
+            $scope.activeId = $scope.list[0].id;
+
+            $scope.clickTab = function(tab){
+                $scope.activeId = tab.id;
+                $scope.tabClick(tab);
+            }
+        }
+    };
+}]);
+
+app.directive('sheet',[function(){
+    return {
+        restrict: 'E',
+        replace: true,
+        templateUrl: 'view/template/sheet.html',
+        scope: {
+            data: '=',
+            visible: '=',
+            select: '&'
+        },
+        link: function($scope){
+            $scope.hideSheet = function(){
+                $scope.visible = false;
+            };
+        }
+    };
+}]);
+
 // app.service('cache',['$cookies',function($cookies){
 //       this.put = function(key,value){
 //           $cookies.put(key,value);
@@ -545,5 +544,24 @@ app.factory('cache',['$cookies',function($cookies){
         remove: function(key){
             $cookies.remove(key);
         }
+    };
+}]);
+
+app.filter('filterByObj',[function(){
+    return function(list,obj){
+        var result = [];
+        angular.forEach(list,function(item){
+
+            var isEqual = true;
+            for(var attr in obj){
+                if(item[attr] !== obj[attr]){
+                    isEqual = false;
+                }
+            }
+            if(isEqual){
+                result.push(item);
+            }
+        });
+        return result;
     };
 }]);
--- src/script/directive/mainList.js
+++ src/script/directive/mainList.js
@@ -1,11 +1,29 @@
-app.directive('mainList',function(){
+app.directive('mainList',['$http',function($http){
     return {
         restrict:'E',
         replace:true,
         templateUrl:"view/template/mainList.html",
         scope: {
             data: '=',
-            filterObj: '='
+            filterObj: '=',
+            isFavorite: '='
+        },
+        link: function($scope,$http){
+
+            $scope.select = function(item,$event){
+
+                // $http.post('data/favorite.json',{
+                //     id: item.id,
+                //     select: !item.select
+                // }).then(function(res){
+                //     console.log(res);
+                //     item.select = !item.select;
+                // },function(err){
+                //     console.log(err)
+                // });
+                item.select = !item.select;
+                $event.stopPropagation();
+            };
         }
     };
-});
+}]);
